ddc109a90be3135dff788dc331f282c680472103
rynturtle <rmclaughlin6666@gmail.com>
2025-12-10 15:49:05 +0000

    changes tabbar colours on tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [TabBar].
-
 void main() => runApp(const TabBarApp());
 
 class TabBarApp extends StatelessWidget {
@@ -9,26 +7,56 @@ class TabBarApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: TabBarExample());
+    return const MaterialApp(home: MainTabBar());
   }
 }
 
-class TabBarExample extends StatelessWidget {
-  const TabBarExample({super.key});
+class MainTabBar extends StatefulWidget {
+  const MainTabBar({super.key});
 
+  @override
+  State<MainTabBar> createState() => _MainTabBarState();
+}
+
+
+class _MainTabBarState extends State<MainTabBar> {
+  int _currentIndex = 1;
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: 4,
+      length: 3,
       child: Scaffold(
-        appBar: AppBar(
+          appBar: AppBar(
           toolbarHeight: 0,
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.calendar_month,size: 40)),
-              Tab(icon: Icon(Icons.notifications_outlined,size: 40)),
-              Tab(icon: Icon(Icons.settings,size: 40)),
+          bottom: TabBar(indicatorColor: Colors.blue,
+          unselectedLabelColor: Color.fromARGB(255, 49, 49, 49),
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+
+          tabs: <Widget>[
+              Tab(icon: Icon(
+                Icons.calendar_month,
+                size: 40,
+                // if current index is the first one then change colour to amber else dont 
+                color: _currentIndex == 0 ? Colors.blue:null,
+              )),
+
+              Tab(icon: Icon(
+                Icons.notifications,
+                size: 40,
+                color: _currentIndex == 1 ? Colors.amber:null,
+              )),
+
+              Tab(icon: Icon(
+                Icons.settings,
+                size: 40,
+                color: _currentIndex == 2 ? const Color.fromARGB(255, 0, 0, 0):null,
+
+              )),
             ],
           ),
         ),
@@ -38,8 +66,6 @@ class TabBarExample extends StatelessWidget {
             Center(child: Text("1")),
             Center(child: Text("2")),
             Center(child: Text("3")),
-            Center(child: Text("4")),
-
           ],
         ),
       ),
